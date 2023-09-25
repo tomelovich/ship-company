6755a5fcef74462c41acf62b51a02141827a8ef1
Alexander Tomelovich <tomelovichs@gmail.com>
2023-09-25 08:01:41 +0300

    Начал курс по Js

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
-const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
-    slidesPerView: "1",
-    touchRatio: "0",
-    loop: "true",
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    autoplay: {
-        delay: 15000,
-        desableOnInteraction:false
-    },
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev'
-    },
+"use strict"; 
+// const swiper = new Swiper('.swiper', {
+//     speed: 400,
+//     spaceBetween: 100,
+//     slidesPerView: "1",
+//     touchRatio: "0",
+//     loop: "true",
+//     effect: 'fade',
+//     fadeEffect: {
+//         crossFade: true
+//     },
+//     autoplay: {
+//         delay: 15000,
+//         desableOnInteraction:false
+//     },
+//     navigation: {
+//         nextEl: '.button-next',
+//         prevEl: '.button-prev'
+//     },
     
-  });
-  
+//   });
+// ------------------------------------------------
+ 
+let incr = 10,
+    decr = 10;
+
+//     ++incr;
+// decr--;
+
+ 
+console.log(++incr);    
+console.log(--decr);    
